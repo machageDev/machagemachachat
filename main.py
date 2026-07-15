5914a03283e8d9673f330da82b5bb530832d4df9
machageDev <machagefranklyn@gmail.com>
2026-07-15 21:46:33 +0300

    login screen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
-from kivymd.app import MDApp
 
 from screens.splash_screen import SplashScreen
 from screens.login_screen import LoginScreen
@@ -9,17 +9,17 @@ from screens.login_screen import LoginScreen
 class MachChat(MDApp):
 
     def build(self):
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.theme_style = "Light"
 
         Builder.load_file("kv/splash.kv")
         Builder.load_file("kv/login.kv")
 
         sm = ScreenManager()
-
         sm.add_widget(SplashScreen(name="splash"))
         sm.add_widget(LoginScreen(name="login"))
 
         return sm
 
 
-if __name__ == "__main__":
-    MachChat().run()
+MachChat().run()
